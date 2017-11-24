affd568ecbe2837ae919fa7948f9c0f68c2c9ecd
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2017-11-24 15:52:18 +0100

    🎉 Else and if

---
+++ 30-else-and-if.py
@@ -0,0 +1,15 @@
+people = 30
+cars = 40
+buses = 15
+
+if (cars > people):
+    print "We should take the cars"
+elif cars < people:
+    print "We may want to take the bus"
+else:
+    print "We can't decide"
+
+if people > buses:
+    print "Alright let's just take the buses"
+else:
+    print "Fine. Let's just stay home then"
