befe3e7a592b7e93abd7519d7a7fa179da6b0e2f
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2017-11-24 15:52:00 +0100

    🎉 What if

---
+++ 29-what-if.py
@@ -0,0 +1,28 @@
+
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print "Too many cats; the world is doomed!"
+
+if people > cats:
+    print "Not many cats; the world is saved!"
+
+if people < dogs:
+    print 'The world is drooled on.'
+
+if people > dogs:
+    print "The world's probably still a little drooled on"
+
+dogs += 5
+
+if people >= dogs:
+    print "People are greater or equal to dogs. Hmm... interesting.."
+
+if people <= dogs:
+    print "People are <= dogs though. That's weird"
+
+if people == dogs:
+    print "I've found the fix!"
+
